b5174eeab587bd7e8962cff4a74cf62882b8b8e5
comp <comp@comps-MBP.hsd1.ca.comcast.net>
2020-08-20 01:58:49 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import Header from "./components/Header";
+// import "./styles.css";
+import "./App.css";
+
+class App extends Component {
+  // state = {
+  //   name: "Hello00"
+  // };
+  state = {
+    categories: [
+      {
+        icon: "city",
+        headline: "Häuser, Industriehallen, Büros",
+        description:
+          "Komplettsanierung innen und außen inkl. zertifizierten Elektroarbeiten (Altbau/ Neubau)"
+      },
+      {
+        icon: "house",
+        headline: "Komplette Dachsanierungen und Dacharbeiten",
+        description: ""
+      },
+      {
+        icon: "house-damage",
+        headline: "Verputzarbeiten/ Rissbeseitigung",
+        description: "jeder Art innen und außen"
+      },
+      {
+        icon: "paint-roller",
+        headline: "Malerarbeiten innen und außen",
+        description: ""
+      },
+      {
+        icon: "brush",
+        headline: "Tapezierarbeiten",
+        description: ""
+      },
+      {
+        icon: "scalpel-path",
+        headline: "Lieferung & Verlegung von Bodenbelägen",
+        description: "Inklusive Bodenausgleich etc."
+      },
+      {
+        icon: "ruler-combined",
+        headline: "Trockenbau/ Innenausbau komplett",
+        description: ""
+      },
+      {
+        icon: "hammer",
+        headline: "Holzarbeiten jeglicher Art",
+        description:
+          "Produktion in Bischofsheim über 2.000 m2 (Einheitlich qm) mit modernstem Maschinenpark zur Holzbearbeitung inkl. 5 Achs CNC"
+      },
+      {
+        icon: "sink",
+        headline: "Individuelle Küchengestaltung",
+        description: "Planung, Produktion und Montage"
+      },
+      {
+        icon: "pencil-ruler",
+        headline: "Planung, Produktion und Montage",
+        description:
+          "Innenarchitektur und Design CAD / Rendering / Illustration durch eigene Innenarchitekten / Designer"
+      }
+    ]
+  };
+
+  render() {
+    let data = this.state.categories.map((city, id) => {
+      let headline = city.headline;
+      let desc = city.description;
+      /* console.log(headline);
+      console.log(desc); */
+      // <ul style={{ color: "red" }}>
+      return (
+        <div key={id}>
+          <ul>
+            <li>{headline}</li>
+            <li>{desc}</li>
+          </ul>
+          <hr />
+        </div>
+      );
+    });
+
+    return (
+      <div className="">
+        <Header />
+        {data}
+      </div>
+    );
+  }
+}
+
+export default App;
